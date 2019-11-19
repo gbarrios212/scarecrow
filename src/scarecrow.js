71bca78c6b20b3e14ae8fa043fa4c47438c7ef5e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-11-19 13:52:27 -0500

    Add scarecrow animations

--- src/scarecrow.js
+++ src/scarecrow.js
@@ -3,10 +3,13 @@ const Bullet = require("./bullet.js");
 const Util = require("./utils.js");
 
 const scarecrowImage = new Image ();
-scarecrowImage.src = "../dist/scarecrow03.png";
+// scarecrowImage.src = "../dist/scarecrow03.png";
+// scarecrowImage.src = "../dist/scarecrow_flying_final.png";
+scarecrowImage.src = "../dist/scarecrow_flying_wide.png";
 
 function Scarecrow(options) {
     MovingObject.call(this, { pos: [400, 400], vel: [0, 0], width: 60, height: 60, image: scarecrowImage, game: options.game, isWrappable: true })
+    // MovingObject.call(this, { pos: [400, 400], vel: [0, 0], width: 480, height: 640, image: scarecrowImage, game: options.game, isWrappable: true })
 }
 
 
@@ -20,4 +23,91 @@ Scarecrow.prototype.fireBullet = function() {
     let bullet = new Bullet({ pos: this.pos, vel: [3, 3], game: this.game, isWrappable: false });
     this.game.bullets.push(bullet);
 }
+
+// Scarecrow.prototype.drawFrame = function (frameX, frameY, cnvasX, canvasY) {
+
+// }
+
+
+// const cycleLoop = [0, 64, 128];
+const cycleLoop = [
+    0, 
+    64, 
+    128, 
+    192, 
+    256, 
+    320, 
+    384, 
+    448, 
+    512, 
+    576, 
+    // 640
+];
+let currentLoopIndex = 0;
+
+
+
+Scarecrow.prototype.step = function() {
+
+    // ctx.clearRect(0, 0, 700, 450);
+    ctx.drawImage(this.image, cycleLoop[currentLoopIndex], 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+    currentLoopIndex++;
+    if (currentLoopIndex >= cycleLoop.length) {
+        currentLoopIndex = 0;
+    }
+    window.requestAnimationFrame(this.step);
+}
+
+// window.requestAnimationFrame(this.step);
+
+Scarecrow.prototype.draw = function () {
+    // ctx.drawImage(this.image, 0, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2); 
+    // ctx.drawImage(this.image, 0, 64, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 0, 128, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 0, 192, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 0, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 64, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 128, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 192, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    //correct below
+    // ctx.drawImage(this.image, 0, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 64, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 128, 0, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    // ctx.drawImage(this.image, 0, 64, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 64, 64, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 128, 64, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    // ctx.drawImage(this.image, 0, 128, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 64, 128, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, 128, 128, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    // ctx.drawImage(this.image, 0, 192, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    //attempted dry 
+    // let one = 0;
+    // let two = 64; 
+    // let three = 128; 
+    // let four = 192;
+
+    // ctx.drawImage(this.image, one, one, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, two, one, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, three, one, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    // ctx.drawImage(this.image, one, two, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, two, two, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, three, two, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    // ctx.drawImage(this.image, one, three, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, two, three, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+    // ctx.drawImage(this.image, three, three, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    // ctx.drawImage(this.image, one, four, this.width, this.height, this.pos[0], this.pos[1], this.width * 2, this.height * 2);
+
+    window.requestAnimationFrame(this.step.bind(this));
+    // this.step();
+}
+
 module.exports = Scarecrow;
